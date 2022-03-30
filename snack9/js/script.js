2a46fc36e6f92e6565c9ec212835773b431b882e
Ilthepriest <robertodelprete1993@libero.it>
2022-03-30 16:10:07 +0200

    add snack9

---
+++ snack9/js/script.js
@@ -0,0 +1,10 @@
+somma = 0;
+media = 0;
+
+for (let i = 1; i <= 10; i++) {
+    somma = somma + i
+    media = somma / 10
+}
+
+console.log(`Somma di 10 numeri ${somma}`);
+console.log(`Media di 10 numeri ${media}`);
