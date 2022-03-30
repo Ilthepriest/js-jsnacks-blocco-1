dfa8114bb673bd1585306d38e48ad13029f0705d
Ilthepriest <robertodelprete1993@libero.it>
2022-03-30 10:23:41 +0200

    add snack1

---
+++ snack1/assets/js/script.js
@@ -0,0 +1,15 @@
+const primo_numero = Number(prompt("Inserisci primo numero"));
+
+console.log(primo_numero);
+
+const secondo_numero = Number(prompt("Inserisci secondo numero"));
+
+console.log(secondo_numero);
+
+if(primo_numero > secondo_numero){
+    console.log(`è maggiore ${primo_numero}`);
+}else{
+    console.log(`è maggiore ${secondo_numero}`);
+}
+
+
