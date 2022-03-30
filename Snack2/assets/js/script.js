06d0b085b898f567077b882dfb4420b823f4a277
Ilthepriest <robertodelprete1993@libero.it>
2022-03-30 10:54:28 +0200

    add snack2

---
+++ Snack2/assets/js/script.js
@@ -0,0 +1,16 @@
+const prima_parola = prompt("Inserisci prima parola");
+
+const seconda_parola = prompt("Inserisci seconda parola");
+
+
+
+if(prima_parola.length < seconda_parola.length){
+    console.log(prima_parola);
+    console.log(seconda_parola);
+    
+}
+
+if(prima_parola.length > seconda_parola.length){
+    console.log(seconda_parola);
+    console.log(prima_parola);
+}
