9c390b53ef2cff6a0ab99a7071db1ce9f2534cad
Ilthepriest <robertodelprete1993@libero.it>
2022-03-30 11:04:06 +0200

    add equal words

--- Snack2/assets/js/script.js
+++ Snack2/assets/js/script.js
@@ -6,11 +6,14 @@ const seconda_parola = prompt("Inserisci seconda parola");
 
 if(prima_parola.length < seconda_parola.length){
     console.log(prima_parola);
-    console.log(seconda_parola);
-    
+    console.log(seconda_parola); 
 }
 
 if(prima_parola.length > seconda_parola.length){
     console.log(seconda_parola);
     console.log(prima_parola);
+}
+
+if(prima_parola.length == seconda_parola.length){
+    console.log("sono uguali");
 }
